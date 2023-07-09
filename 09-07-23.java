41800a4c8bbcdcf5fb3b027bac0b278dab0ae34a
Yogi1710 <103318661+yogi1710@users.noreply.github.com>
2023-07-09 12:17:34 +0530

    Create 09-07-23.java

---
+++ 09-07-23.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class cardealer {
+			public String carmodel;
+			public String insurance;
+			public String add_accessories;
+			public String discount;
+			public long RTO=113990;
+			public long TCSCharges=11000;
+			public long discount_amt=0;
+			public void getCarModel()
+			{
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Select car model: ");
+				carmodel=sc.nextLine();
+				System.out.println("Do you need insurance");
+				insurance=sc.nextLine();
+				System.out.println("Do you need Additional Accessories: ");
+				add_accessories=sc.nextLine();
+				System.out.println("Dealer discount: ");
+				discount=sc.nextLine();
+				sc.close();
+			}
+				
+			public void getCost(String carName,long cost)
+			{
+				long total=cost+RTO+TCSCharges;
+				int insurance_amt=0;
+				int add_accessories_amt=0;
+				if (insurance.equals("yes") || insurance.equals("YES")){
+					insurance_amt=47300;
+					total+=insurance_amt;
+				}
+				if (add_accessories.equals("yes") || insurance.equals("YES")) {
+					add_accessories_amt=15000;
+					total+=add_accessories_amt;
+				}
+				if (discount.endsWith("%")) {
+					discount=discount.replace("%","");
+					discount_amt=Integer.valueOf(discount)*cost/100;
+				}
+				else
+					discount_amt=Integer.valueOf(discount);
+				if(discount_amt>30000) {
+					System.out.println("Maximum discount should not cross 30000!");
+				}
+				if(!insurance.equalsIgnoreCase("yes") && !add_accessories.equalsIgnoreCase("yes") && discount_amt > 0) {
+					System.out.println("Any one of the additional features have to be added!");
+					discount_amt=0;
+				}
+				if(insurance.equalsIgnoreCase("yes") || add_accessories.equalsIgnoreCase("yes") && discount_amt <= 30000) {
+					total-=discount_amt;
+				}
+				System.out.println();
+				System.out.println("Total cost "+total+"("+carmodel+" "+cost+" + "+RTO+"(RTO) + "+insurance_amt+"(Insurance) + "+TCSCharges+"(TCS) + "+add_accessories_amt+"(Additional Accessories) - "+discount_amt+"(Dealer discount))");
+		
+			}
+	}
+
+
+public class cardealeruse extends cardealer{
+	public static void main(String[] args) {
+		cardealeruse cdu=new cardealeruse();
+		String[] arr= new String[]{"Polo Trendline","Polo Highline","Virtus Trendline","Virtus Highline","Taigun Trendline","Taigun Highline","Taigun Topline"};
+		long[] cost=new long[]{870000,1009000,1105000,1308000,1489000,1542000,1771000};
+		cdu.getCarModel();
+		for(int i=0;i<arr.length;i++)
+	  {
+			
+			if(arr[i].equals(cdu.carmodel))
+			{
+				cdu.getCost(arr[i],cost[i]);
+				
+			}
+		}
+	}
+}
